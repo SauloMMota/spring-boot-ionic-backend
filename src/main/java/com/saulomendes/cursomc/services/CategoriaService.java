73e736d7940176006f26a0c35e8e26634e7d35b5
Saulo Mendes <saulomendes_br@hotmail.com>
2021-05-06 20:47:27 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/saulomendes/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.saulomendes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saulomendes.cursomc.domain.Categoria;
+import com.saulomendes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);		
+		return obj.orElse(null);
+	}
+}
