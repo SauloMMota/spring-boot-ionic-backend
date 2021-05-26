67dd6fa4e7c6a98cbef2d7c599fbb6dfa3bd189c
Saulo Mendes <saulomendes_br@hotmail.com>
2021-05-25 22:26:12 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/saulomendes/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.saulomendes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saulomendes.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
