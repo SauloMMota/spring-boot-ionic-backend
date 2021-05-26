1e951116b98f3fd1bb447f6fccb7d9efe03426f5
Saulo Mendes <saulomendes_br@hotmail.com>
2021-05-26 20:39:28 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/saulomendes/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.saulomendes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saulomendes.cursomc.domain.Cliente;
+import com.saulomendes.cursomc.domain.Pedido;
+import com.saulomendes.cursomc.repositories.PedidoRepository;
+import com.saulomendes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido>  obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
